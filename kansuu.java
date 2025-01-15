6d858f5fb27b69bbb72d3025f6e1f66b274ab6ac
kana sasazawa <kana.yellow7@gmail.com>
2025-01-15 22:37:51 +0900

    関数エラー

---
+++ kansuu.java
@@ -0,0 +1,24 @@
+
+public class kansuu {
+
+	public static void main(String[] args) {
+		// 関数
+		
+		//戻り値の型　関数名　（引数の型　引数１,・・)　｛
+		//処理１;
+		//処理２;
+		//return 戻り値
+		
+		//引数がなくても関数の外にかけば表示可能
+		public static string hello () {
+			return "こんにちは";
+		}
+		System.out.println ("山田さん。"+hello());
+		System.out.println("<br>");
+		System.out.println ("鈴木さん。"+hello());
+		System.out.println("<br>");
+		System.out.println ("佐藤さん。"+hello());
+		System.out.println("<br>");
+	}
+
+}
