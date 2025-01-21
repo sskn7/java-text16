ddaa05a4fa9eb137ac30bfa361a71e54ae143c8a
kana sasazawa <kana.yellow7@gmail.com>
2025-01-21 23:07:02 +0900

    関数成功

--- kansuu.java
+++ kansuu.java
@@ -1,24 +1,27 @@
-
 public class kansuu {
 
+	// 関数
+	
+	//戻り値の型　関数名　（引数の型　引数１,・・)　｛
+	//処理１;
+	//処理２;
+	//return 戻り値
+	
+	public static String hello() {
+		return "こんにちは";
+	}
+	
+	
 	public static void main(String[] args) {
-		// 関数
-		
-		//戻り値の型　関数名　（引数の型　引数１,・・)　｛
-		//処理１;
-		//処理２;
-		//return 戻り値
-		
-		//引数がなくても関数の外にかけば表示可能
-		public static string hello () {
-			return "こんにちは";
-		}
-		System.out.println ("山田さん。"+hello());
+		System.out.println ("山田さん。" + hello());
 		System.out.println("<br>");
 		System.out.println ("鈴木さん。"+hello());
 		System.out.println("<br>");
 		System.out.println ("佐藤さん。"+hello());
 		System.out.println("<br>");
+		
 	}
+	
+	
 
 }
---
+++ kansuu02.java
@@ -0,0 +1,16 @@
+
+public class kansuu02 {
+	
+	private static int triple (int num) {
+		return num * 3;
+	}
+	
+
+	public static void main(String[] args) {
+		System.out.println(triple(10));
+		System.out.println(triple(4) * 3);
+		
+
+	}
+
+}
