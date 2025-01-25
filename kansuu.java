b9909a462c1c458b1eca0aa21d941c110231c18b
kana sasazawa <kana.yellow7@gmail.com>
2025-01-25 22:27:30 +0900

    消費税、配送料を計算する

--- kansuu.java
+++ kansuu.java
@@ -11,6 +11,16 @@ public class kansuu {
 		return "こんにちは";
 	}
 	
+	// 全ての引数を指定するメソッド 
+	public static int totalprice(int fruitprice, double tax, int haiso) { 
+		return (int) (fruitprice * tax) + haiso; }
+	
+	// taxを1.08に固定したメソッド 
+	public static int totalprice(int fruitprice) { 
+		return totalprice(fruitprice, 1.08, 350); 
+		// 配送料もデフォルト値として設定 
+		}
+	
 	
 	public static void main(String[] args) {
 		System.out.println ("山田さん。" + hello());
@@ -20,6 +30,10 @@ public class kansuu {
 		System.out.println ("佐藤さん。"+hello());
 		System.out.println("<br>");
 		
+		System.out.println(totalprice(300) + "<br>");
+		System.out.println(totalprice(450) + "<br>");
+		System.out.println(totalprice(400) + "<br>");
+		System.out.println(totalprice(350) + "<br>");
 	}
 	
 	
